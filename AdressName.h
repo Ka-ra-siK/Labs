3e29b5a768624ae1e509b7e45ded0ec54eb70161
Konstantin Konovalov <53082951+Ka-ra-siK@users.noreply.github.com>
2020-12-02 16:18:55 +0700

    Update AdressName.h

--- AdressName.h
+++ AdressName.h
@@ -4,12 +4,14 @@
 class AdressName: public Bankomat {
 public:
 	AdressName();
-	AdressName(const char* name, const char* adress, const char* id, int _curr_amount);
+	AdressName(const char* adress, const char* name, const char* id, int _curr_amount);
+	AdressName(const AdressName& bank, const Bankomat& bankOr);
 	~AdressName();
 	void setName(const char* name);
 	void setAdress(const char* adress);
 	char* getName();
 	char* getAdress();
+	char* toString();
 	friend istream& operator >> (istream& in, AdressName& bank);
 	friend ostream& operator << (ostream& out, AdressName& bank);
 private:
